3ce76e37e2c99ef95d484e9d708b85a67dd62a60
Brian Q Taylor <btaylor2555@gmail.com>
2023-08-02 18:50:16 -0400

    addded 4 divs and started the javascript

---
+++ Assets/js/script.js
@@ -0,0 +1,26 @@
+// what should i set as my globally variables 
+var hideQuestion = document.querySelector("#hide")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is my click button that will hide my my start screen
+hideQuestion.addEventListener("click", )
+
